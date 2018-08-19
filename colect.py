95b668b13a04777ffb5039798db88d5b75193fd1
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2018-08-19 12:11:49 -0300

    Add colect data

--- colect.py
+++ colect.py
@@ -1,16 +1,29 @@
 from hcsr04 import HCSR04
+from motorShield import MotorShield
 import time, math, machine
 
 sensor = HCSR04(trigger_pin=16, echo_pin=0)
+
 servo = machine.PWM(machine.Pin(12), freq=50)
+
+distance = sensor.distance_cm()
+
 i = 30
-#Leitura em 180 graus
-while i<136: 
-    print(i)
-    servo.duty(i)
-    time.sleep(1)
-    i = i + 21
+while True:
 
+    distance = sensor.distance_cm()
+    print(distance)
+    if distance<5:
+        while i<136: 
+            servo.duty(i)
+            distance2 = sensor.distance_cm()
+            print('distancia',distance2)
+            time.sleep(1)
+            print(i)
+            i = i + 21
 
+    else:
+        i = 30
+       
     
     
