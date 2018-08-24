3e32af113419ca33cdbb7de61eba80d52be28cca
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2018-08-24 16:58:03 -0300

    corecting code

--- colect.py
+++ colect.py
@@ -1,6 +1,28 @@
 from hcsr04 import HCSR04
 import time, math, machine
 
+"""
+    Colect data for a ultrasonic sensor in 180 degrees
+    Copyright (C) 2018  William Ribeiro (willcribeiro)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    contact :   https://github.com/willcribeiro or
+                 william-cribeiro@bct.ect.ufrn.br
+
+    """
+
 sensor = HCSR04(trigger_pin=16, echo_pin=0)
 servo = machine.PWM(machine.Pin(12), freq=50)
 distance = sensor.distance_cm()
