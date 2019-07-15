9eaa2482002a8f6a8bac8653fed4408f7db8f7ba
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2019-07-15 19:09:27 -0300

    update main move

--- Control of Motor/main.py
+++ Control of Motor/main.py
@@ -2,14 +2,15 @@
 from motorShield import MotorShield
 import time, math, machine
 
-rodas = MotorShield()
-rodas.directionRobot(1, 1)
-rodas.vel(600,600)
-time.sleep(3)
-rodas.directionRobot(1, 0)
-rodas.vel(600,600)
-time.sleep(1)
-rodas.directionRobot(1, 1)
-rodas.vel(600,600)
-time.sleep(3)
-rodas.vel(0,0)
+def movimento():
+    rodas = MotorShield()
+    rodas.directionRobot(1, 1)
+    rodas.vel(600,600)
+    time.sleep(3)
+    rodas.directionRobot(1, 0)
+    rodas.vel(600,600)
+    time.sleep(1)
+    rodas.directionRobot(1, 1)
+    rodas.vel(600,600)
+    time.sleep(3)
+    rodas.vel(0,0)
